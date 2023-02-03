3528983e48496b608d26c2f3d46ea7dc8e4e0142
Belphin <findolph0003@gmail.com>
2023-02-03 16:21:16 +0200

    Main classes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import BoardComponent from "./components/BoardComponent";
+import { Board } from "./models/Board";
 
 function App() {
-	return <div>Work</div>;
+	const [board, setBoard] = useState(new Board());
+
+	useEffect(() => {
+		restart();
+	}, []);
+
+	function restart() {
+		const newBoard = new Board();
+		newBoard.initCells();
+		newBoard.addFigures();
+		setBoard(newBoard);
+	}
+
+	return (
+		<div>
+			<BoardComponent board={board} setBoard={setBoard} />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,35 @@
+import { Colors } from "../Colors";
+import logo from "../../assets/black-king.png";
+import { Cell } from "../Cell";
+
+export enum FigureNames {
+	FIGURE = "Фігура",
+	KING = "Король",
+	KNIGHT = "Кінь",
+	PAWN = "Пішак",
+	QUEEN = "Ферзь",
+	ROOK = "Тура",
+	BISHOP = "Слон",
+}
+
+export class Figure {
+	color: Colors;
+	logo: typeof logo | null;
+	cell: Cell;
+	name: FigureNames;
+	id: number;
+
+	constructor(color: Colors, cell: Cell) {
+		this.color = color;
+		this.cell = cell;
+		this.cell.figure = this;
+		this.logo = null;
+		this.name = FigureNames.FIGURE;
+		this.id = Math.random();
+	}
+
+	canMove(target: Cell): boolean {
+		return true;
+	}
+	moveFigure(target: Cell) {}
+}
