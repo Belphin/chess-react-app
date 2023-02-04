278a364833fab0862df1856c829bf54504df8914
Belphin <findolph0003@gmail.com>
2023-02-04 14:13:16 +0200

    alternate move

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,14 +1,22 @@
 import React, { FC, useState, useEffect } from "react";
 import { Board } from "../models/Board";
 import { Cell } from "../models/Cell";
+import { Player } from "../models/Player";
 import CellComponent from "./CellComponent";
 
 interface BoardProps {
 	board: Board;
 	setBoard: (board: Board) => void;
+	currentPlayer: Player | null;
+	swapPlayer: () => void;
 }
 
-const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
+const BoardComponent: FC<BoardProps> = ({
+	board,
+	setBoard,
+	currentPlayer,
+	swapPlayer,
+}) => {
 	const [selectedCell, setSelectedCell] = useState<Cell | null>(null);
 
 	function click(cell: Cell) {
@@ -18,8 +26,11 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
 			selectedCell.figure?.canMove(cell)
 		) {
 			selectedCell.moveFigure(cell);
+			swapPlayer();
 			setSelectedCell(null);
-		} else setSelectedCell(cell);
+		} else {
+			if (cell.figure?.color === currentPlayer?.color) setSelectedCell(cell);
+		}
 	}
 
 	useEffect(() => highLightCells(), [selectedCell]);
@@ -35,22 +46,25 @@ const BoardComponent: FC<BoardProps> = ({ board, setBoard }) => {
 	}
 
 	return (
-		<div className="board">
-			{board.cells.map((row, index) => (
-				<React.Fragment key={index}>
-					{row.map((cell) => (
-						<CellComponent
-							click={click}
-							selected={
-								cell.x === selectedCell?.x && cell.y === selectedCell?.y
-							}
-							cell={cell}
-							key={cell.id}
-						/>
-					))}
-				</React.Fragment>
-			))}
-		</div>
+		<>
+			<h3>{currentPlayer?.color} move</h3>
+			<div className="board">
+				{board.cells.map((row, index) => (
+					<React.Fragment key={index}>
+						{row.map((cell) => (
+							<CellComponent
+								click={click}
+								selected={
+									cell.x === selectedCell?.x && cell.y === selectedCell?.y
+								}
+								cell={cell}
+								key={cell.id}
+							/>
+						))}
+					</React.Fragment>
+				))}
+			</div>
+		</>
 	);
 };
 
